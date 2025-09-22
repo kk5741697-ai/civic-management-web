61aed0384cdd309ec60e7daaba984a1cd6301a2a
kk5741697-ai <kk5741697@gmail.com>
2025-09-23 00:08:01 +0600

    upload theme folder

---
+++ themes/civicgroup/info.php
@@ -0,0 +1,13 @@
+<?php
+$themeName = 'Vrbel';
+
+$themeFolder = 'vrbel';
+
+$themeAuthor = 'Aminul Islam';
+
+$themeAuthorUrl = 'mailto:admin@vrbel.com';
+
+$themeVirsion = '1.0';
+
+$themeImg = $themeFolder . '/themeLogo.png';
+?>
